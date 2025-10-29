de52c8ef0627e5eab2188c79513056cd0bd33b35
Gyasuddin <gyasuddinansari7854@gmail.com>
2025-10-29 16:39:57 +0530

    Readme.md add and output

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://locahlost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 }   
 );
